a2d9ed5025d20d6ac1581630915f5a657a7465e2
Dhnzhan <gdh00549@naver.com>
2025-12-15 17:39:55 +0900

    Feat: login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -3,8 +3,15 @@ import * as s from "./styles";
 import { FcGoogle } from "react-icons/fc";
 import { SiNaver } from "react-icons/si";
 import { RiKakaoTalkFill } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+
+  const handleOAuth2LoginOnClick = (e) => {
+    const clientName = e.target.id;
+    navigate("http://localhost:8080/oauth2/authorization/" + clientName);
+  };
   return (
     <div css={s.layout}>
       <div css={s.container}>
@@ -13,15 +20,19 @@ function Login() {
           <h1>Social Board</h1>
           <p>친구들과 함께 사진 또는 글을 공유해보세요.</p>
           <div>
-            <button css={s.google}>
+            <button
+              id="google"
+              css={s.google}
+              onClick={handleOAuth2LoginOnClick}
+            >
               <FcGoogle />
               구글 로그인
             </button>
-            <button css={s.naver}>
+            <button id="naver" css={s.naver} onClick={handleOAuth2LoginOnClick}>
               <SiNaver />
               네이버 로그인
             </button>
-            <button css={s.kakao}>
+            <button id="kakao" css={s.kakao} onClick={handleOAuth2LoginOnClick}>
               <RiKakaoTalkFill />
               카카오 로그인
             </button>
