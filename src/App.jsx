0c58ab923338e9cda291d4fd6dd01cbe6d705f0d
Dhnzhan <gdh00549@naver.com>
2025-12-15 16:56:22 +0900

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MainLayout from "./components/common/MainLayout";
+import AuthRoute from "./routes/AuthRoute";
+
+function App() {
+  return (
+    <MainLayout>
+      <AuthRoute />
+    </MainLayout>
+  );
+}
+
+export default App;
